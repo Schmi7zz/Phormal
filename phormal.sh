afd6b7701e3cf728f8978157e529312115e30fba
Schmitz <34347069+Schmi7zz@users.noreply.github.com>
2026-06-23 17:46:14 +0330

    Update phormal.sh

--- phormal.sh
+++ phormal.sh
@@ -15,7 +15,7 @@ set -Eeuo pipefail
 # ------------------------------------------------------------------------------
 #  Constants
 # ------------------------------------------------------------------------------
-readonly PHORMAL_VERSION="5.6.5"
+readonly PHORMAL_VERSION="6.2.2"
 readonly PHORMAL_SPEED_PORT=15987
 readonly PHORMAL_HOME="/etc/phormal"
 readonly PHORMAL_CONF="${PHORMAL_HOME}/phormal.conf"
